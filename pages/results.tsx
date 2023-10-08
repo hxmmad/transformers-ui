80df39fb0c37e647a0eac50fe76af2d04a85d242
hxmmad <44508704+hxmmad@users.noreply.github.com>
2023-10-07 17:57:02 -0700

    fix

--- pages/results.tsx
+++ pages/results.tsx
@@ -4,6 +4,7 @@ import { Grid, Box, Typography, Paper, Button, Skeleton, Card, Fab, Switch, Togg
 import Image from 'next/image';
 import { NavigateBefore } from '@mui/icons-material';
 import zIndex from '@mui/material/styles/zIndex';
+import Link from 'next/link';
 
 interface Answer {
   text: string;
@@ -75,12 +76,14 @@ export default function Results() {
     </Grid>
   <Grid container xs={5} direction='column' sx={{display:'flex', height:'100%', zIndex:1, alignItems:'center',}}>
     <Grid item sx={{display: 'flex', justifyContent:'center'}} xs="auto">
+     <Link href="/"> 
       <Paper className="rounded-2xl textmain" elevation={24} sx={{marginTop:'8vh', display:'flex', height:'10vh', width:'43vh', position:'absolute', justifyContent:'center', alignItems:'center', overflow:'hidden'}}>
       <video autoPlay loop muted style={{ position: 'relative', top: 0, left: 0, width: '100%', height: '100%', objectFit: 'cover', }}>
               <source src="/videos/night.mp4" type="video/mp4" />
             </video>
   <Typography sx={{position:'absolute', fontSize:'3.5vh'}} className="textmain outfit font-extrabold">transformers <span className="textalt">|</span> <span className="textmain">vectordb</span></Typography>
   </Paper>
+  </Link>
       </Grid>
       <Grid container direction="column" spacing={1} sx={{display:'flex', height: '100%', position: 'relative'}}>
     <input
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  // Serve static files from the "public" directory
+  server.use(express.static('public'));
+
+  server.all('*', (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(3000, (err) => {
+    if (err) throw err;
+    console.log('> Ready on http://localhost:3000');
+  });
+}).catch(err => {
+  console.log('Error:::::', err);
+});
